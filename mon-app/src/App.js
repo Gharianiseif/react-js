2a4bd04c5bdac759e476a3d6561c924ea332554c
seif <gharianisipon@gmail.com>
2021-04-07 10:28:03 +0200

    mon-app

---
+++ mon-app/src/App.js
@@ -0,0 +1,18 @@
+
+import './App.css';
+import First from './component/NavBar/First';
+import Second from './component/NavBar/Second';
+function App() {
+  return (
+    <div className="App">
+      <h1> React</h1>
+      <First />
+      <Second/>
+  
+    </div>
+
+
+  );
+}
+
+export default App;
